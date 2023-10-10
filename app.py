c1cec9316ca1154949d3905ba8610ebd4e07b175
Amulyapriyaeamani <145454104+Amulyapriyaeamani@users.noreply.github.com>
2023-10-10 20:31:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ def extract_text_from_pdf(pdf_file):
         pdf_reader = PyPDF2.PdfReader(pdf_file)
         text = ""
         for page_num in range(len(pdf_reader.pages)):
-            text += pdf_reader.pages[page_num].extract_Text()
+            page = pdf_reader.pages[page_num]
+            text += page.extract_Text()
         return text
 
 # Function to convert text to speech
